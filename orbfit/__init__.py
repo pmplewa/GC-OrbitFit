244412fbdf97d06329e23414a00d41e88b11222a
pmplewa <pmplewa@users.noreply.github.com>
2021-04-22 14:52:56 +0200

    Always log to console

--- orbfit/__init__.py
+++ orbfit/__init__.py
@@ -1,6 +1,8 @@
 import logging
+import sys
 
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    stream=sys.__stdout__) # print to console even in a notebook session
